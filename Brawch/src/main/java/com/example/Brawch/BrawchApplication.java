9a955db82640578715eef58c93edd26ddb65c3ff
youngrae <youngrae1103@gmail.com>
2025-04-15 19:21:23 +0900

    test

---
+++ Brawch/src/main/java/com/example/Brawch/BrawchApplication.java
@@ -0,0 +1,13 @@
+package com.example.Brawch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrawchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrawchApplication.class, args);
+	}
+
+}
---
+++ Brawch/src/test/java/com/example/Brawch/BrawchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Brawch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrawchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
