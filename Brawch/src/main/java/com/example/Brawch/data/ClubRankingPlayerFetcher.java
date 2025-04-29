ea0ff924eea4bf1029825b40b9ce3b0888b2b54f
youngrae <youngrae1103@gmail.com>
2025-04-29 15:07:27 +0900

    test

--- Brawch/src/main/java/com/example/Brawch/data/ClubRankingPlayerFetcher.java
+++ Brawch/src/main/java/com/example/Brawch/data/ClubRankingPlayerFetcher.java
@@ -9,14 +9,14 @@ import com.google.gson.*;
 public class ClubRankingPlayerFetcher {
 
     // Bearer 뒤에 자기 API TOKEN 넣어주기 Bearer 지우면 안됨!
-    private static final String API_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRlZTc1M2UyLTQwM2EtNDY3ZS1hOTA0LWZjMTVkZmMxN2E5MiIsImlhdCI6MTc0NDY1MDUxMiwic3ViIjoiZGV2ZWxvcGVyL2UzYWI0ZGM4LWI3NzctODMyZi04YTRhLTBkMTAzZDQyMWE0MCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMzUuMTk0LjY1LjkxIl0sInR5cGUiOiJjbGllbnQifV19.GP5Q3wYjJ8uliJ1529GC5pfw7mH4z7aJxFBa_3DFSUon4mzogUUiE1XOnq9b473UeVv25rpOszmhShOyCHfOvw";
+    private static final String API_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA1YmRkOTI3LWE5NTQtNDEwNC04NTQyLTYxMjA2NjdjNTM1NCIsImlhdCI6MTc0NTg4ODM1MSwic3ViIjoiZGV2ZWxvcGVyL2UzYWI0ZGM4LWI3NzctODMyZi04YTRhLTBkMTAzZDQyMWE0MCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMjExLjIzOC4xMDkuMTY4Il0sInR5cGUiOiJjbGllbnQifV19.wbsZSs7vCPBPv-VyHb97dmm0BOL7DZdQLX8GeEfljInC4-kKJbbBBHjh_FzXGqs8aJCLoGrY6U1pfMqbplJJ5Q";
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/testdb";
     //db 이름 정해서 aws로 진행할때 이름으로 수정하기
     private static final String DB_USER = "postgres";
     // 유저명
     private static final String DB_PASSWORD = "1111";
     // 설정 비밀번호
-    // ⭐⭐ 추가: 실행할 때 작업자가 자신의 batch_id를 지정
+    // ** 추가: 실행할 때 작업자가 자신의 batch_id를 지정
     // 조영래 : 1, 이예성 : 2, 강영우 : 3
     private static final int BATCH_ID = 1; // ← 실행자별로 1, 2, 3 지정
 
